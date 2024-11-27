58e279cf42036e1d8fc5d754ecfe59e2b3ad7d94
Antonio Vázquez Montalban <antuane.vazquez04@gmail.com>
2024-11-26 22:38:18 -0600

    ui logic

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import LoginForm from './components/LoginForm'
+import Header from './components/Header'
+import Menu from './components/Menu'
+
+
+function App() {
+
+  return (
+    <>
+      <LoginForm/>
+      <Header/>
+      <Menu/>
+    </>
+    
+  )
+}
+
+export default App
